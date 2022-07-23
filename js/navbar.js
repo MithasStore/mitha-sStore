a0981a08a7df8fcaa30e5383c45e7a02552ef6ca
mithaax07@gmail.com <mithaax07@gmail.com>
2022-07-23 14:59:58 +0700

    added

--- js/navbar.js
+++ js/navbar.js
@@ -1,43 +1,19 @@
-const body = document.querySelector("body"),
-  nav = document.querySelector("nav"),
-  modeToggle = document.querySelector(".dark-light"),
-  searchToggle = document.querySelector(".searchToggle"),
-  sidebarOpen = document.querySelector(".sidebarOpen"),
-  siderbarClose = document.querySelector(".siderbarClose");
+const hamburger = document.querySelector(".hamburger"),
+  navMenu = document.querySelector("nav"),
+  searchToggle = document.querySelector(".searchToggle");
 
-let getMode = localStorage.getItem("mode");
-if (getMode && getMode === "dark-mode") {
-  body.classList.add("dark");
-}
-
-// js code to toggle dark and light mode
-modeToggle.addEventListener("click", () => {
-  modeToggle.classList.toggle("active");
-  body.classList.toggle("dark");
-
-  // js code to keep user selected mode even page refresh or file reopen
-  if (!body.classList.contains("dark")) {
-    localStorage.setItem("mode", "light-mode");
-  } else {
-    localStorage.setItem("mode", "dark-mode");
-  }
-});
-
-// js code to toggle search box
+// ===== SEARCH TOGGLE =====
 searchToggle.addEventListener("click", () => {
   searchToggle.classList.toggle("active");
 });
 
-
-//   js code to toggle sidebar
-sidebarOpen.addEventListener("click", () => {
-  nav.classList.add("active");
+// ===== OPEN SIDEBAR MENU =====
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-body.addEventListener("click", e => {
-  let clickedElm = e.target;
-
-  if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
-    nav.classList.remove("active");
-  }
-});
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
